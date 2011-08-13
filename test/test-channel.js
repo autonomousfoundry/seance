ad5abd238dca65afc4d60b562e7adac1a7c1ea2a
Brian Auton <brianauton@gmail.com>
2011-08-13 12:24:18 -0400

    Allow initial loading of a page in the Phantom channel

--- test/test-channel.js
+++ test/test-channel.js
@@ -1,6 +1,7 @@
 var channel = require("../lib/channel");
+var http = require("http");
 
-exports["creates a two-way channel of communication with PhantomJS"] = function(test) {
+exports["creates a two-way channel of communication with a PhantomJS WebPage object"] = function(test) {
     var c = channel.create();
     c.send("console.log('one');");
     c.once("output", function(data) {
@@ -11,5 +12,21 @@ exports["creates a two-way channel of communication with PhantomJS"] = function(
             c.close();
             test.done();
         });
+    });
+};
+
+exports["loads a URL into the WebPage upon creation, if specified"] = function(test) {
+    var server = http.createServer(function(req, res) {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end("<html><body></body></html>");
+    });
+    server.listen("6543");
+    var c = channel.create("http://localhost:6543/");
+    c.send("console.log(document.location.href)");
+    c.once("output", function(data) {
+        test.equal("http://localhost:6543/", data);
+        server.close();
+        c.close();
+        test.done();
     });
 };
