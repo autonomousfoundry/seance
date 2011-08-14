af1b96e1e7f1804fc07f0f066d031d9af10549d9
Brian Auton <brianauton@gmail.com>
2011-08-14 10:03:53 -0400

    Return Phantom pages directly instead of via callback

--- test/test-seance.js
+++ test/test-seance.js
@@ -3,11 +3,10 @@ var http = require("http");
 
 exports["attaches to a node http server and signals when ready"] = function(test) {
     var server = http.createServer();
-    seance.attach(server).on("ready", function(page) {
-        page.close();
-        server.close();
-        test.done();
-    });
+    var page = seance.attach(server);
+    page.close();
+    server.close();
+    test.done();
 };
 
 exports["executes javascript in the page context"] = function(test) {
@@ -15,12 +14,11 @@ exports["executes javascript in the page context"] = function(test) {
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end("<html><head><title>TheTitle</title></head><body></body></html>");
     });
-    seance.attach(server).on("ready", function(page) {
-        page.evaluate("document.title", function(value) {
-            test.equal("TheTitle", value);
-            page.close();
-            server.close();
-            test.done();
-        });
+    var page = seance.attach(server);
+    page.evaluate("document.title", function(value) {
+        test.equal("TheTitle", value);
+        page.close();
+        server.close();
+        test.done();
     });
 };
